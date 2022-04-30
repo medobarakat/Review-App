c049ce86be79401d42e40718a3a40b189ce796d9
medobarakat <ahmedbarakat2401@gmail.com>
2022-04-30 10:50:37 +0200

    adding features

--- components/Header.js
+++ components/Header.js
@@ -1,27 +1,32 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, Image, ImageBackground } from "react-native";
 import * as React from "react";
 import { StyleSheet } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 
-const Header = ({ navigation, menu, main, about }) => {
+const Header = ({ navigation, main, about }) => {
   const menuHandler = () => {
     navigation.openDrawer();
   };
   return (
-    <View style={HeaderStyles.container}>
-      {menu && (
-        <TouchableOpacity style={HeaderStyles.icon}>
-          <MaterialIcons name="menu" size={28} onPress={menuHandler} />
-        </TouchableOpacity>
-      )}
-
-      {/* <TouchableOpacity style={HeaderStyles.icon}>
-        <MaterialIcons name="menu" size={28} onPress={menuHandler} />
-      </TouchableOpacity> */}
-
-      {main && <Text style={HeaderStyles.txt}>Game Zone </Text>}
-      {about && <Text style={HeaderStyles.txt}>About Game Zone </Text>}
-    </View>
+    <ImageBackground
+      source={require("../assets/game_bg.png")}
+      style={HeaderStyles.container}
+    >
+      <MaterialIcons
+        name="menu"
+        size={28}
+        onPress={menuHandler}
+        style={HeaderStyles.icon}
+      />
+      <View style={HeaderStyles.header}>
+        <Image
+          source={require("../assets/heart_logo.png")}
+          style={HeaderStyles.img}
+        />
+        {main && <Text style={HeaderStyles.txt}>Game Zone </Text>}
+        {about && <Text style={HeaderStyles.txt}>About Game Zone </Text>}
+      </View>
+    </ImageBackground>
   );
 };
 
@@ -34,16 +39,23 @@ const HeaderStyles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
-    flex: 1,
+  },
+  header: {
+    flexDirection: "row",
   },
   txt: {
     letterSpacing: 1,
-    fontSize: 20,
+    fontSize: 25,
     fontFamily: "Nunito-bold",
     color: "#333",
   },
   icon: {
     position: "absolute",
     left: 5,
+  },
+  img: {
+    width: 26,
+    height: 26,
+    marginHorizontal: 10,
   },
 });
